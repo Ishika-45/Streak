b02bc1f2433d776a125b85f04452be79f7836c8f
Ishika-45 <bansalishika58@gmail.com>
2025-11-15 11:53:47 +0530

    Check String is Palindrome or not

---
+++ Recursion/stringPalindrome.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class stringPalindrome {
+
+        public static boolean isPalindrome(String str) {
+            int left = 0;
+            int right = str.length() - 1;
+
+            while (left <= right) {
+                if (str.charAt(left) != str.charAt(right))
+                    return false;
+
+                left++;
+                right--;
+            }
+            return true;
+        }
+    
+}
