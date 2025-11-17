43f8c047a96132bdaeea777773fb758664803fdc
Ishika-45 <bansalishika58@gmail.com>
2025-11-17 22:22:36 +0530

    Print the pairs of subarray

---
+++ Array/printPairs.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+
+public class printPairs {
+    public static void printPairs(int[] arr){
+        int totalPairs = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("Pairs in array: (" + arr[i] + " , " + arr[j] + ")");
+                totalPairs++;
+            }
+        }
+        System.out.println(totalPairs);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        printPairs(arr);
+//        Another way to print totalPairs
+        int totalPairs = arr.length * (arr.length - 1) / 2;
+        System.out.println("Total pairs: " + totalPairs);
+    }
+}
