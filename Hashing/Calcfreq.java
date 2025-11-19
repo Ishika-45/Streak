5d36c9c28a200d8ebe96e1df0a6f88e6f74654e7
Ishika-45 <bansalishika58@gmail.com>
2025-11-20 05:22:36 +0530

    Modify the approach

--- Hashing/Calcfreq.java
+++ Hashing/Calcfreq.java
@@ -1,18 +1,42 @@
-package Hashing;
+import java.util.*;
 
-public class calcFreq {
-    public static int[] calcFreq(int n, int[] arr){
-        int[] hash = new int[n];
+public class Calcfreq {
 
-        for (int i = 0; i < arr.lenth; i++) {
-            if(arr[i] <= n){
-                hash[arr[i] - 1]++;
-            }
-            if(hash[i] < hash[i+1]){
-                max = hash[i+1];
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        Frequency(arr, n);
+    }
+    static void Frequency(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
             }
         }
-        return hash;
 
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        // Traverse through map and find the elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = element;
+                minFreq = count;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
     }
-}
+} 
