eb9f94b49da5611b587a7fa60baaf739b0badd09
Ishika-45 <bansalishika58@gmail.com>
2025-11-24 20:11:45 +0530

    Complete the code

--- Array/PrintSubarrays.java
+++ Array/PrintSubarrays.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Arrays;
 
-public class printSubarrays {
+public class PrintSubarrays {
     public static void printSubarrays(int[] arr){
         int totalPairs = 0;
         for (int i = 0; i < arr.length; i++) {
