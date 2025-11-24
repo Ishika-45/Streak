0cab3aa7749b2507cfd92d2bd11ab6f0fb2156ff
Ishika-45 <bansalishika58@gmail.com>
2025-11-24 20:07:15 +0530

    Complete the code

--- Array/Smallest.java
+++ Array/Smallest.java
@@ -1,4 +1,20 @@
 package Array;
 
 public class Smallest {
+
+    static int findSmallest(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,34,56,75,2};
+        int smallest = findSmallest(arr);
+        System.out.println("The smallest element in an array: " + smallest);
+    }
 }
