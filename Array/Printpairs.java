1cd492de1cc4dd383ce895a9442e4d6995c6b870
Ishika-45 <bansalishika58@gmail.com>
2025-11-24 20:11:32 +0530

    Complete the code

--- Array/Printpairs.java
+++ Array/Printpairs.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Arrays;
 
-public class printPairs {
+public class Printpairs {
     public static void printPairs(int[] arr){
         int totalPairs = 0;
         for (int i = 0; i < arr.length; i++) {
