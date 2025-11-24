8bbab1e6469e4312414d20c1ec13ddf9700b69fc
Ishika-45 <bansalishika58@gmail.com>
2025-11-24 20:06:57 +0530

    Complete the code

--- Array/Secsmallest.java
+++ Array/Secsmallest.java
@@ -1,4 +1,25 @@
 package Array;
 
 public class Secsmallest {
+
+    static int findSecondSmallest(int[] arr){
+        int smallest = Integer.MAX_VALUE;
+        int secSmallest = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(smallest > arr[i]){
+                secSmallest = smallest;
+                smallest = arr[i];
+            }
+            if(arr[i] < secSmallest && arr[i] != smallest){
+                secSmallest = arr[i];
+            }
+        }
+        return secSmallest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,5,7,7,23,45};
+        int secSmallest = findSecondSmallest(arr);
+        System.out.println("Second Smallest: " + secSmallest);
+    }
 }
