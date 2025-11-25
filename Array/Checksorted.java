8a803db18140524a807246e0cc4fc55a0a40d1c7
Ishika-45 <bansalishika58@gmail.com>
2025-11-25 20:09:40 +0530

    Complete the code

--- Array/Checksorted.java
+++ Array/Checksorted.java
@@ -1,4 +1,23 @@
 package Array;
 
 public class Checksorted {
+
+    static boolean isSorted(int[] arr){
+        int start = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i-1] < arr[i]){
+
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,0,5,0,1,2};
+        boolean bool = isSorted(arr);
+        System.out.println("The given array is sorted or not: " + bool);
+    }
 }
