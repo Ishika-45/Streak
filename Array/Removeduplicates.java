e13787ef1a031083013d85bb78a12db1b29e1c18
Ishika-45 <bansalishika58@gmail.com>
2025-11-26 19:06:25 +0530

    Complete the code

--- Array/Removeduplicates.java
+++ Array/Removeduplicates.java
@@ -1,4 +1,41 @@
 package Array;
 
+import java.util.HashSet;
+
 public class Removeduplicates {
+    static int removeDuplicate(int[] nums){
+//        Brute-Force
+//        HashSet<Integer> unique = new HashSet<>();
+//        int index = 0;
+//        for(int num : nums){
+//            if(!unique.contains(num)){
+//                unique.add(num);
+//                nums[index] = num;
+//                index++;
+//            }
+//        }
+//        return index;
+
+        if(nums.length == 0) return 0;
+
+        int start = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[start]){
+                start++;
+                nums[start] = nums[i];
+            }
+        }
+        return start + 1;
+    }
+    public static void main(String[] args) {
+        int  arr[]= {1,1,2,2,2,3,3};
+
+        int index = removeDuplicate(arr);
+        System.out.println("Unique elements: " + index);
+        System.out.print("Original array after removing duplicates: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
