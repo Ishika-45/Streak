2d8b5f461dc0e905a5c432aad94ac4379f9f743d
Ishika-45 <bansalishika58@gmail.com>
2025-11-27 23:06:38 +0530

    Number appears twice or once

---
+++ Array/Numappeartwice.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Numappeartwice {
+    static int appearsOnce(int[] arr){
+        int count = 1;
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if(arr[i] == arr[j]){
+                count++;
+            }
+            if(count == 1){
+                return arr[i];
+            }
+            i++;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,1,2};
+        int countOne = appearsOnce(arr);
+        System.out.println("The number which appears once in the array is: " + countOne);
+    }
+}
