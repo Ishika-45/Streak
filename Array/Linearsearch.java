5911f764a992084a59d911d67f9f6f6760af7f0b
Ishika-45 <bansalishika58@gmail.com>
2025-11-27 11:36:10 +0530

    Linear search in an array

---
+++ Array/Linearsearch.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Linearsearch {
+    static int linearSearch(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int target = 3;
+        int index = linearSearch(arr, target);
+        System.out.print("The target element " + target + " is found at index: " + index);
+    }
+}
