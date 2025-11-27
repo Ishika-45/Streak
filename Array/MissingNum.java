826d02a01d54556a6c4103f12925e48e832bf0ca
Ishika-45 <bansalishika58@gmail.com>
2025-11-27 22:19:51 +0530

    Missing number from the sorted array

---
+++ Array/MissingNum.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class MissingNum {
+    static int missNum(int[] arr, int n){
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum - arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        int[] arr = {1,3};
+        int missingNumber = missNum(arr,n);
+        System.out.println("The missing number in the array is: " + missingNumber);
+    }
+}
