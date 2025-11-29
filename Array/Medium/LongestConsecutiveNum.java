7871c8989f90a17c53dfb1530a7946036de159d9
Ishika-45 <bansalishika58@gmail.com>
2025-11-29 23:01:07 +0530

    Longest Consecutive number

---
+++ Array/Medium/LongestConsecutiveNum.java
@@ -0,0 +1,13 @@
+package Array.Medium;
+
+public class LongestConsecutiveNum {
+    static int longestConsecutiveSequence(int[] arr){
+        int num = 0;
+       return num;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,3,7,2,5,8,4,6,0,1};
+        int longestSequence = longestConsecutiveSequence(arr);
+        System.out.println("Longest Consecutive Sequence is: " + longestSequence);
+    }
+}
